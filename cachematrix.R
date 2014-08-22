8583a65f38e43cce66e0b7e424a860a5aabe66e1
Felipe Aristizabal <felipe.aristizabal@mail.mcgill.ca>
2014-08-22 10:50:37 -0400

    Initial modifications to makeCacheMatrix. cacheSolve function is a copy of the example for vector mean.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R:  
+## 
+##
+## Felipe Aristizabal 
+## cursera: R Programming
+## Peer Assignment 2
+## August 22, 2014
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix: Write a short comment describing this function
 
-}
+makeCacheMatrix <- function(mat = matrix()) {
+	inv <- NULL
+	set <- function(y) {
+		mat <<- y
+		inv <<- NULL
+	}
+	get <- function() mat
+	setinv <- function(usrInv) inv <<- usrInv 
+	getinv <- function() inv
+	list(set = set, get = get,
+		setinv = setinv,
+		getinv = getinv)
+	}
 
 
-## Write a short comment describing this function
+## cacheSolve: Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+	m <- x$getmean()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- mean(data, ...)
+	x$setmean(m)
+	m
         ## Return a matrix that is the inverse of 'x'
 }
